82de1380080ec9bb264033c9b19f97bae3418764
tian3401 <matt.chen3401@yahoo.com>
2018-07-08 17:08:15 -0700

    fully working gif generator

--- app.js
+++ app.js
@@ -6,22 +6,19 @@ newButton;
 //Create function that creates new buttons for topics in the array
 function buttonCreator(subject){
     
-    var newButton = $('<button>').text(topics[x]);
+    newButton = $('<button>').text(topics[x]);
     $('#buttonContainers').append(newButton);
     }
 
- 
 for(x in topics){
     buttonCreator(topics[x]);
 }; 
 
-
 // Create function that creates new buttons for inputs
-
 $('form button').on('click', function(){
     event.preventDefault();
     var userInput = $('input').val();
-    var newButton = $('<button>').text(userInput);
+    newButton = $('<button>').text(userInput);
 
     $('#buttonContainers').append(newButton); 
     topics.push(userInput);
@@ -29,7 +26,6 @@ $('form button').on('click', function(){
 }); 
 
 //Create function on button click that pings the Giphy api and appends to the page
-
 $(document).on('click','#buttonContainers button', function(){
     clear();
     var pressed = $(this).text();
@@ -37,11 +33,10 @@ $(document).on('click','#buttonContainers button', function(){
 
     $.ajax({
         url: url
-    }).then(function(res){
+    }).then(function call (res){
         for(x=0;x<10;x++){
-            newImg = $('<img>').attr('src',res.data[x].images.downsized_still.url).attr('id', 'gif');
-            newButton = $('<button>').attr('data-motion',res.data[x].images.downsized.url).attr('data-toggle','off');
-            console.log(res.data);
+            newImg = $('<img>').attr('src',res.data[x].images.fixed_height_still.url).attr('id', 'gif');
+            newButton = $('<button>').attr('data-motion',res.data[x].images.downsized.url).attr('data-still',res.data[x].images.downsized_still.url).attr('data-toggle','off');
             newButton.append(newImg);
             $('#gifContainer').append(newButton);
         }
@@ -52,24 +47,24 @@ $(document).on('click','#buttonContainers button', function(){
 $(document).on('click', '#gifContainer button', function(){
     var button = $(this);
     var toggle = button.data('toggle');
+    
     console.log(toggle);
-    if(toggle === 'off'){
-        
+    if(toggle == 'off'){
         var pressed = $(this).data('motion');
-        console.log('if statement runs');
-        newImage = $('<img>').attr('src',pressed);
+        var still = $('#gif').data('still');
+        console.log(still);
+        newImage = $('<img>').attr('src',pressed).attr('data-still',still);
+        button.removeData('toggle');
         button.attr('data-toggle','on').html(newImage);
     }
-    if(toggle==='on'){
-        console.log('runs else statement');
+    else if(toggle =='on'){
+        button.removeData('toggle');
+        var still = $(this).data('still');
+        newImage = $('<img>').attr('src',still).attr('id','gif');
+        console.log(newImage);
+        button.attr('data-toggle','off').html(newImage);
     }
-    
-
-    
-
-
 });
-
 
 // Clears the gif container so buttons replace instead of append 
 // Keeps the number of gifs to 10 in the gif container 
